e096c3656e06c71acd3c6f0bdec6c4ba0b34152c
Robertas Kudalevas <robertas.kudalevas@gmail.com>
2024-10-24 15:25:56 +0300

    Added basic layout, login and register page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('home.index');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
